279d5815a82009fcaffb7b8cf8b056bf870e44ce
stealcompass <venya.e@gmail.com>
2022-12-05 19:14:34 +0300

    Add new function to Presenter

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -9,10 +9,30 @@ import Foundation
 import UIKit
 
 
-final class MovieQuizPresenter {
+final class MovieQuizPresenter: QuestionFactoryDelegate{
     
     private var currentIndex: Int = 0
     let questionsAmount: Int = 10
+    var correctCount: Int = 0
+    
+    var currentQuestion: QuizQuestion? // аналогично, делаем через композицию
+    weak var viewController : MovieQuizViewController? // слабая ссылка на вьюконтроллер
+
+    
+    var questionFactory: QuestionFactoryProtocol?
+    
+    
+    init(viewController: MovieQuizViewController) {
+        self.viewController = viewController
+        self.questionFactory = QuestionFactory(delegate: self, moviesLoader: MoviesLoader()) //здесь инициализируем фабрику
+        
+        questionFactory?.loadData()// начинаем загрузку - внутри loadData() в зависимости от состояния вызываются функции didLoadDataFromServer() и didFailToLoadData()
+        
+    }
+
+    
+    private var statisticService: StatisticService? = StatisticServiceImplementation()
+    
     
     //вынесли всю логику работы с переменными этого класса для использования в MQVC
     func isLastQuestion() -> Bool{
@@ -24,12 +44,11 @@ final class MovieQuizPresenter {
     }
     
     func switchToNextQuestion(){
-        currentIndex+=1
+        currentIndex += 1
     }
     
     
-    
-    
+
     
     // функция перевода данных из исходного вида QuizQuestion в QuizStepViewModel для последующего отображения на экране
     func convert(model: QuizQuestion) -> QuizStepViewModel {
@@ -40,38 +59,95 @@ final class MovieQuizPresenter {
     }
     
     
-    
-    var currentQuestion: QuizQuestion? // аналогично, делаем через композицию
-    weak var viewController : MovieQuizViewController? // слабая ссылка на вьюконтроллер
-    
-    
     func yesButtonClicked(){
-        
-        guard let currentQuestion = currentQuestion else {
-            return
-        }
-        
-        if currentQuestion.correctAnswer {
-            viewController?.showAnswerResult(isCorrect: true)
-        } else {
-            viewController?.showAnswerResult(isCorrect: false)
-        }
+        didAnswer(isYes: true)
     }
     
     
     func noButtonClicked(){
+        didAnswer(isYes: false)
+    }
+    
+    
+    func didAnswer(isYes: Bool){
         guard let currentQuestion = currentQuestion else {
             return
         }
         
-        if currentQuestion.correctAnswer{
-            viewController?.showAnswerResult(isCorrect: false)
-        } else {
-            viewController?.showAnswerResult(isCorrect: true)
+        let currentAnswer = isYes
+        viewController?.showAnswerResult(isCorrect: currentAnswer == currentQuestion.correctAnswer)
+    }
+    
+    
+    
+    //  - QuestionFactoryDelegate -  реализация метода из протокола
+    func didReceiveNextQuestion(question: QuizQuestion?) {
+        
+        guard let question = question else {
+            return
         }
         
+        currentQuestion = question
+        let viewModel = convert(model: question)
+        DispatchQueue.main.async{ [weak self] in
+            self?.viewController?.hideLoadingIndicator()
+            self?.viewController?.show(quiz: viewModel)
+        }
         
+    }
+    
+    
+    // основная функция, которая отвечает за логику того,
+    //что будет показано на экране в зависимости от номера текущего вопроса
+    func showNextQuestionOrResults() {
         
+        if self.isLastQuestion(){
+            
+            statisticService?.store(correct: correctCount, total: self.questionsAmount)
+            guard let gamesCount = statisticService?.gamesCount else {return}
+            guard let bestGame = statisticService?.bestGame else {return}
+            guard let totalAccuracy = statisticService?.totalAccuracy else {return}
+            
+            let viewModel = AlertModel(title: "Этот раунд закончен",
+                                       text: "Ваш результат: \(correctCount)/\(self.questionsAmount)\nКоличество сыгранных квизов: \(gamesCount)\nРекорд: \(bestGame.correct)/\(bestGame.total) \(bestGame.date.dateTimeString) \nСредняя точность: \(String(format: "%.2f", totalAccuracy))%",
+                                       buttonText: "Сыграть еще раз") {
+                
+                self.resetQuestionIndex()
+                self.correctCount = 0
+                self.questionFactory?.requestNextQuestion()
+            }
+
+            viewController?.showAlertFin(viewModel: viewModel)
+            
+        } else {
+            
+            self.switchToNextQuestion()
+            
+            viewController?.showLoadingIndicator()
+            questionFactory?.requestNextQuestion()
+        }
+    }
+    
+    func didCorrectAnswer(isCorrectAnsw: Bool){
+        if isCorrectAnsw {
+            self.correctCount += 1
+        }
+    }
+    
+    func restartGame(){
+        self.correctCount = 0
+        self.currentIndex = 0
+        self.resetQuestionIndex()
+    }
+    
+    
+    func didLoadDataFromServer() {//скрываем индикатор и показываем новый экран
+        viewController?.showLoadingIndicator()
+        questionFactory?.requestNextQuestion()
+    }
+    
+    func didFailToLoadData(with error: Error) { // функция для отображения ошибки
+        viewController?.showNetworkError(message: error.localizedDescription)
     }
     
     
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,19 +1,9 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
+final class MovieQuizViewController: UIViewController {
     
-    private var correctCount = 0
-
-    private var questionFactory: QuestionFactoryProtocol? // связываем контроллер и класс QuestionFactory через "композицию", создавая экземпляр внутри контроллера. // также прописываем это свойство не напрямую через класс, а через протокол в котором описана функция// после добавления делегата в фабрику - необходимо его указывать, но через self этого сделать нельзя, тк свойство еще не инициализировано
-    
-    private var currentQuestion: QuizQuestion? // аналогично, делаем через композицию
     private var alert: AlertPresenterProtocol?
-    private var statisticService: StatisticService?
-    
-    
-    private let presenter = MovieQuizPresenter()
-    
-    
+    private var presenter: MovieQuizPresenter!
 
     
     
@@ -27,92 +17,56 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        self.imageView.layer.cornerRadius = 20
         
-        questionFactory = QuestionFactory(delegate: self, moviesLoader: MoviesLoader() ) //здесь инициализируем фабрику
+        self.imageView.layer.cornerRadius = 20
+    
+        presenter = MovieQuizPresenter(viewController: self)
         
         self.showLoadingIndicator() // показываем индикатор
         
-        questionFactory?.loadData() // начинаем загрузку - внутри loadData() в зависимости от состояния вызываются функции didLoadDataFromServer() и didFailToLoadData()
-        
-        
-        presenter.viewController = self
-        //questionFactory?.requestNextQuestion()
-        
         alert = AlertPresenter(controller: self)
-        
-        statisticService = StatisticServiceImplementation()
     }
     
     
     
     //функция для отображения индикатора
-    private func showLoadingIndicator() {
+    func showLoadingIndicator() {
         activityIndicator.isHidden = false // говорим, что индикатор загрузки не скрыт
         activityIndicator.startAnimating() // включаем анимацию
     }
     
-    private func hideLoadingIndicator(){
+    func hideLoadingIndicator(){
         activityIndicator.isHidden = true // говорим, что индикатор загрузки скрыт
-        activityIndicator.stopAnimating() // включаем анимацию
+        activityIndicator.stopAnimating() // выключаем анимацию
     }
     
+    
+    
     //функция отображения ошибки
-    private func showNetworkError(message: String) {
+    func showNetworkError(message: String) {
         hideLoadingIndicator() // скрываем индикатор загрузки
-        
-        self.presenter.resetQuestionIndex()
-        self.correctCount = 0
         
         // создайте и покажите алерт
         let viewModel = AlertModel(title: "Ошибка",
                                    text: message,
                                    buttonText: "Попробовать еще раз") {[weak self] in
             guard let self = self else {return}
-            self.alert?.restartGame()
+            self.presenter.restartGame()
         }
-        alert?.showAlert(result: viewModel)
-    }
-    
-    
-    func didLoadDataFromServer() {//скрываем индикатор и показываем новый экран
-        activityIndicator.isHidden = true
-        questionFactory?.requestNextQuestion()
-    }
-    
-    func didFailToLoadData(with error: Error) { // функция для отображения ошибки
-        showNetworkError(message: error.localizedDescription)
+        
+        showAlertFin(viewModel: viewModel)
     }
     
     
     // работа с 2мя кнопками
     @IBAction private func noButtonClicked(_ sender: Any) {
-        presenter.currentQuestion = currentQuestion
         presenter.noButtonClicked()
     }
     
     @IBAction private func yesButtonClicked(_ sender: Any) {
-        presenter.currentQuestion = currentQuestion
         presenter.yesButtonClicked()
     }
     
-    
-    //  - QuestionFactoryDelegate -  реализация метода из протокола
-    func didReceiveNextQuestion(question: QuizQuestion?) {
-        
-        
-        
-        guard let question = question else {
-            return
-        }
-        
-        currentQuestion = question
-        let viewModel = presenter.convert(model: question)
-        DispatchQueue.main.async{ [weak self] in
-            self?.show(quiz: viewModel)
-        }
-        
-    }
     
     
     // функция отображения информации о правильности/ неправильности ответа на текущий вопрос
@@ -122,8 +76,9 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
         imageView.layer.borderWidth = 8
         imageView.layer.cornerRadius = 20
         
+        presenter.didCorrectAnswer(isCorrectAnsw: isCorrect)
+        
         if isCorrect {
-            correctCount += 1
             imageView.layer.borderColor = UIColor.ypGreen.cgColor
         } else {
             imageView.layer.borderColor = UIColor.ypRed.cgColor
@@ -131,53 +86,24 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){ [weak self] in
             guard let self = self else {return}
-            self.showNextQuestionOrResults()
-        }
-    }
-
-    
-    
-    // основная функция, которая отвечает за логику того, что будет показано на экране в зависимости от номера текущего вопроса
-    private func showNextQuestionOrResults() {
-        if  presenter.isLastQuestion(){
-            
-            statisticService?.store(correct: correctCount, total: presenter.questionsAmount)
-            guard let gamesCount = statisticService?.gamesCount else {return}
-            guard let bestGame = statisticService?.bestGame else {return}
-            guard let totalAccuracy = statisticService?.totalAccuracy else {return}
-            
-            let viewModel = AlertModel(title: "Этот раунд закончен",
-                                       text: "Ваш результат: \(correctCount)/\(presenter.questionsAmount)\nКоличество сыгранных квизов: \(gamesCount)\nРекорд: \(bestGame.correct)/\(bestGame.total) \(bestGame.date.dateTimeString) \nСредняя точность: \(String(format: "%.2f", totalAccuracy))%",
-                                       buttonText: "Сыграть еще раз") {
-                
-                self.presenter.resetQuestionIndex()
-                self.correctCount = 0
-                self.questionFactory?.requestNextQuestion()
-            }
             
             self.imageView.layer.borderWidth = 0
-            alert?.showAlert(result: viewModel)
-            
-            
-        } else {
-            presenter.switchToNextQuestion()
-            
-            self.showLoadingIndicator()
-            questionFactory?.requestNextQuestion()
-            self.hideLoadingIndicator()
-            
-            self.imageView.layer.borderWidth = 0
-            
+            self.presenter.showNextQuestionOrResults()
         }
     }
     
     
     //показываем новую картинку
-    private func show(quiz step: QuizStepViewModel) {
+     func show(quiz step: QuizStepViewModel) {
         // здесь мы заполняем нашу картинку, текст и счётчик данными
         imageView.image =  step.image
         counterLabel.text = step.questionNumber
         textLabel.text = step.question
+    }
+    
+    
+    func showAlertFin(viewModel: AlertModel){
+        alert?.showAlert(result: viewModel)
     }
 }
 
